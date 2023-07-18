9348c452af2aa0ee1871b916dc481f5aaf611a24
o-yuji <py_ootsuka@yahoo.co.jp>
2023-07-18 21:46:50 +0900

    crawlSpaider

--- kinokuniya/spiders/computer_books.py
+++ kinokuniya/spiders/computer_books.py
@@ -1,18 +1,19 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
+import logging
 
 class ComputerBooksSpider(CrawlSpider):
     name = "computer_books"
     allowed_domains = ["www.kinokuniya.co.jp"]
     start_urls = ["https://www.kinokuniya.co.jp/f/dsd-101001037028005-06-"]
 
-    rules = (Rule(LinkExtractor(allow=r"Items/"), callback="parse_item", follow=True),)
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='//h3[@class="heightLine-2"]/a'),
+             callback="parse_item", follow=False),
+        Rule(LinkExtractor(restrict_xpaths = '(//a[contains(text(),"次へ")])[1]'),
+             follow=True)
+        )
 
     def parse_item(self, response):
-        item = {}
-        #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
-        #item["name"] = response.xpath('//div[@id="name"]').get()
-        #item["description"] = response.xpath('//div[@id="description"]').get()
-        return item
+        logging.info(response.url)
