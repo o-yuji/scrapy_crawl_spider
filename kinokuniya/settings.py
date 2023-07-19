a8aa8fb36ce7728ecf77c7f618d6b5364f7b339b
o-yuji <py_ootsuka@yahoo.co.jp>
2023-07-19 13:42:43 +0900

    crawl-by-spider

--- kinokuniya/settings.py
+++ kinokuniya/settings.py
@@ -20,7 +20,10 @@ NEWSPIDER_MODULE = "kinokuniya.spiders"
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
+DEPTH_PRIORITY = 1
+SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
+SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
--- kinokuniya/spiders/computer_books.py
+++ kinokuniya/spiders/computer_books.py
@@ -15,5 +15,39 @@ class ComputerBooksSpider(CrawlSpider):
              follow=True)
         )
 
+    def get_title(self,title):
+        if title:
+            return ' '.join(title).lstrip()
+        return title
+
+    def get_price(self,price):
+        if price:
+            return int(price.replace('¥','').replace(',',''))
+        return 0
+    
+    def get_size(self,size):
+        if size:
+            return size.split('／')[0].replace('サイズ ','').replace('判','')
+        return size
+
+    def get_page(self,page):
+        if page:
+            return int(page.split('／')[1].replace('ページ数 ','').replace('p',''))
+        return 0
+    
+    def get_isbn(self,isbn):
+        if isbn:
+            return isbn.replace('商品コード ','')
+        return isbn
+
     def parse_item(self, response):
         logging.info(response.url)
+        yield {
+            'title':self.get_title(response.xpath('//h3[@itemprop="name"]/text()').getall()),
+            'author':response.xpath('//div[@class="infobox ml10 mt10"]/ul/li[1]/a/text()').get(),
+            'price':self.get_price(response.xpath('//span[@class="sale_price"]/text()').get()),
+            'publisher':response.xpath('//a[contains(@href,"publisher-key")]/text()').get(),
+            'size':self.get_size(response.xpath('normalize-space(//div[contains(@class,"infbox")]/ul/li[1]/text())').get()),
+            'page':self.get_page(response.xpath('normalize-space(//div[contains(@class,"infbox")]/ul/li[1]/text())').get()),
+            'isbn':self.get_isbn(response.xpath('//li[@itemprop="identifier"]/text()').get()),
+        }
